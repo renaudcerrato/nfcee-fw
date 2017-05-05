b9654bcb206f0d853698f667ff7bded76667a137
Renaud Cerrato <renaud.cerrato@gmail.com>
2017-05-05 11:25:17 -0400

    fixed wrong indentation

--- main.c
+++ main.c
@@ -10,34 +10,34 @@
 static THD_WORKING_AREA(waThread1, 128);
 static THD_FUNCTION(Thread1, arg) {
 
-  (void)arg;
-  chRegSetThreadName("blinker");
-  while (true) {
-    gpioSetPad(GPIO_LED0);
-    chThdSleepMilliseconds(500);
-    gpioClearPad(GPIO_LED0);
-    chThdSleepMilliseconds(500);
-  }
+    (void)arg;
+    chRegSetThreadName("blinker");
+    while (true) {
+        gpioSetPad(GPIO_LED0);
+        chThdSleepMilliseconds(500);
+        gpioClearPad(GPIO_LED0);
+        chThdSleepMilliseconds(500);
+    }
 }
 
 void testInit(void)
 {
-  /*
-   * Creates the example thread.
-   */
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+    /*
+     * Creates the example thread.
+     */
+    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 }
 
 int main(void) {
 
-  halInit();
-  chSysInit();
+    halInit();
+    chSysInit();
 
-  testInit();
-	trace("[NFCEE] - Hello world\r\n");
-  while (true) {
-    chThdSleepMilliseconds(500);
-  }
+    testInit();
+    trace("[NFCEE] - Hello world\r\n");
+    while (true) {
+        chThdSleepMilliseconds(500);
+    }
 }
 
 
